fe824ecc6c633ce5479c4420a5e1d04d7d68fe03
Phillip Madden <symbolicon@live.com>
2025-12-08 10:27:55 +0800

    Day 1 Morning Break

---
+++ src/Module1/MyApp1/Global.cs
@@ -0,0 +1,21 @@
+﻿using C = System.Console;
+using MsgBox = System.Windows.Forms.MessageBox;
+
+namespace Symbolicon.Examples
+{
+	struct Global
+	{
+		static void Main()
+		{
+			// This content will not be compiled until end-of-line
+			/* Any content within here will not be compiled */
+
+			C.WriteLine("Hello!");
+			C.WriteLine("Goodbye!");
+			C.Write("Press any key to continue...");
+			C.ReadKey(true);
+			MsgBox.Show("Click OK to continue.");
+		}
+	}
+}
+
