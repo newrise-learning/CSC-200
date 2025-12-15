f1c1e01191ab9b07a3b3fed14c58bdaa68d89879
Phillip Madden <symbolicon@live.com>
2025-12-15 12:39:39 +0800

    Day 4 Lunch Break

---
+++ src/Module4/DB1/Program.cs
@@ -0,0 +1,21 @@
+﻿using SymBank.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DB1 {
+	class Program {
+		static void Main(string[] args) {
+			var item = new Account {
+				ID = 300,
+				Name = "XYZ PTE LTD",
+				Balance = 50000m
+			};
+			var dc = new SymBankDataContext();
+			dc.Accounts.InsertOnSubmit(item);
+			dc.SubmitChanges();
+		}
+	}
+}
